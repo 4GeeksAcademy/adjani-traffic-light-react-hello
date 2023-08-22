794f49f78e466eee62628bb36e73172397d282c5
Adjani <133295232+Adjani09@users.noreply.github.com>
2023-08-22 22:45:35 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [light, setLight] = useState(0)
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">Traffic Light!</h1>
+			<div className="d-flex flex-column bg-dark col-1 p-2 mx-auto">
+				<div><button onClick={() => setLight(1)} className={`p-5 rounded-full rounded-circle ${light === 1 ? "bg-danger" : "bg-secondary"}`} ></button></div>
+				<div><button onClick={() => setLight(2)} className={`p-5 rounded-full rounded-circle ${light === 2 ? "bg-warning" : "bg-secondary"}`} ></button></div>
+				<div><button onClick={() => setLight(3)} className={`p-5 rounded-full rounded-circle ${light === 3 ? "bg-success" : "bg-secondary"}`} ></button></div>
+			</div>
 		</div>
 	);
 };
-
 export default Home;
