260df2d1c35ef0c71d347c7f3042acec208aad4e
Adjani <133295232+Adjani09@users.noreply.github.com>
2023-09-09 07:08:26 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,9 +10,7 @@ const Home = () => {
 			<h1 className="text-center mt-5">Traffic Light!</h1>
 			<div className="container flex-column" style={{width: 150, backgroundColor:"#082b4e", borderRadius: 30, padding: 10}}>
 				<div><button style={{marginBottom: 10, padding: 50, borderRadius: 70, backgroundColor: light === 1 ? "#ff2222" : "#ccc", boxShadow: light === 1 ? "0 0 20px 10px rgba(255, 34, 34, 0.4)" : "none"}} onClick={() => setLight(1)}></button></div>
-				
 				<div><button style={{marginBottom: 10, padding: 50, borderRadius: 70, backgroundColor: light === 2 ? "#f7b71d" : "#ccc", boxShadow: light === 2 ? "0 0 20px 10px rgba(247, 183, 29, 0.3)" : "none"}} onClick={() => setLight(2)}></button></div>
-
 				<div><button style={{ padding: 50, borderRadius: 70, backgroundColor: light === 3 ? "#09d308" : "#ccc", boxShadow: light === 3 ? "0 0 20px 10px rgba(9, 211, 8, 0.3)" : "none"}} onClick={() => setLight(3)}></button></div>
 			</div>
 		</div>
