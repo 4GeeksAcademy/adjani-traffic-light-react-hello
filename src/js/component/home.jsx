6c5d59d77fd2e716ed586afd5d56da8430094949
Adjani <133295232+Adjani09@users.noreply.github.com>
2023-09-09 07:05:16 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
@@ -9,10 +8,12 @@ const Home = () => {
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">Traffic Light!</h1>
-			<div className="d-flex flex-column bg-dark col-1 p-2 mx-auto">
-				<div><button onClick={() => setLight(1)} className={`p-5 rounded-full rounded-circle ${light === 1 ? "bg-danger" : "bg-secondary"}`} ></button></div>
-				<div><button onClick={() => setLight(2)} className={`p-5 rounded-full rounded-circle ${light === 2 ? "bg-warning" : "bg-secondary"}`} ></button></div>
-				<div><button onClick={() => setLight(3)} className={`p-5 rounded-full rounded-circle ${light === 3 ? "bg-success" : "bg-secondary"}`} ></button></div>
+			<div className="container flex-column" style={{width: 150, backgroundColor:"#082b4e", borderRadius: 30, padding: 10}}>
+				<div><button style={{marginBottom: 10, padding: 50, borderRadius: 70, backgroundColor: light === 1 ? "#ff2222" : "#ccc", boxShadow: light === 1 ? "0 0 20px 10px rgba(255, 34, 34, 0.4)" : "none"}} onClick={() => setLight(1)}></button></div>
+				
+				<div><button style={{marginBottom: 10, padding: 50, borderRadius: 70, backgroundColor: light === 2 ? "#f7b71d" : "#ccc", boxShadow: light === 2 ? "0 0 20px 10px rgba(247, 183, 29, 0.3)" : "none"}} onClick={() => setLight(2)}></button></div>
+
+				<div><button style={{ padding: 50, borderRadius: 70, backgroundColor: light === 3 ? "#09d308" : "#ccc", boxShadow: light === 3 ? "0 0 20px 10px rgba(9, 211, 8, 0.3)" : "none"}} onClick={() => setLight(3)}></button></div>
 			</div>
 		</div>
 	);
